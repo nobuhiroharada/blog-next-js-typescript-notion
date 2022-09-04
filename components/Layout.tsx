eb921bb752db51eb76345a89d0316d0413eed7c1
nobu <nobu@rak-3.local>
2022-09-04 12:32:40 +0900

    LayoutにProps追加

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { FC, ReactNode } from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { LayoutPropsType } from '../types/LayoutPropsType'
 
-const Layout = () => {
+const Layout: FC<LayoutPropsType> = ({ children }) => {
   return (
         <div className="relative overflow-hidden">
             <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
                 <Navbar />
-                <main></main>
+                <main className="w-full pb-12 px-4">{children}</main>
                 <Footer />
             </div>
         </div>
